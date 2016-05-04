03c6c81d91d5009ada046b5536e469908d5ec9d3
izumin5210 <masayuki@izumin.info>
2016-05-04 09:58:30 +0900

    Rename class: Konashi -> Device

--- ota-updater/src/main/java/jp/hxs/android/konashi/otaupdater/domain/entity/Device.java
+++ ota-updater/src/main/java/jp/hxs/android/konashi/otaupdater/domain/entity/Device.java
@@ -3,13 +3,13 @@ package jp.hxs.android.konashi.otaupdater.domain.entity;
 /**
  * Created by izumin on 5/3/2016 AD.
  */
-public class Konashi {
-    public static final String TAG = Konashi.class.getSimpleName();
+public class Device {
+    public static final String TAG = Device.class.getSimpleName();
 
     public final String name;
     public final String revision;
 
-    public Konashi(String name, String revision) {
+    public Device(String name, String revision) {
         this.name = name;
         this.revision = revision;
     }
